a8c40de05e9c7651bb49bc787c7c07704b6d0ced
chudzick <chudzick.andrzej@gmail.com>
2024-07-07 14:28:26 +0200

    Dodanie formularza dodawania książki. Dodanie możliwości filtrowania listy książek

--- movie/urls.py
+++ movie/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_movies, name='all_movies_url'),
     path('<id>', views.movie_details, name='movie_details_url'),
+    path('add', views.add_movie, name='add_movie_url')
 ]
--- movie/views.py
+++ movie/views.py
@@ -6,12 +6,19 @@ from django.db.models import Avg, Min, Max, Count
 
 # Create your views here.
 def all_movies(request):
-    found_movies = Movie.objects.all()
+    title = request.GET.get('title')
+
+    if title and len(title) > 3:
+        found_movies = Movie.objects.filter(original_title__contains=title)
+    else:
+        found_movies = Movie.objects.all()
+
     found_movies_aggregation = found_movies.aggregate(Avg('statistics__vote_average'), Min('statistics__vote_average'),
                                                       Max('statistics__vote_average'), Count('id'))
     context = {
         'movies': found_movies,
-        'aggregation_data': found_movies_aggregation
+        'aggregation_data': found_movies_aggregation,
+        'filter_title': title
     }
     return render(request, 'movie/all_movies.html', context)
 
@@ -24,3 +31,7 @@ def movie_details(request, id):
         'movie': found_movie
     }
     return render(request, 'movie/movie_details.html', context)
+
+
+def add_movie(request):
+    return render(request, 'movie/add_movie.html')
