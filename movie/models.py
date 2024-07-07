386b1f21ece2a8ae2241c91becca6721bd9754e1
chudzick <chudzick.andrzej@gmail.com>
2024-07-07 10:27:03 +0200

    Dodanie ładowania początkowych danych filmu. Napisanie własnej migracji do bazy. Korzystanie z modelu Movie w widokach views.py

--- movie/models.py
+++ movie/models.py
@@ -12,23 +12,3 @@ class Movie(models.Model):
     release_date = models.DateField()
     vote_count = models.IntegerField()
     vote_average = models.DecimalField(max_digits=5, decimal_places=2)
-
-    @staticmethod
-    def find_all_movies():
-        movies_list = []
-
-        with open('movie/migrations/movies_small.csv', 'r', encoding='utf-8') as all_movies_file:
-            reader = csv.DictReader(all_movies_file, delimiter=',')
-
-            for row in reader:
-                movies_list.append(Movie(
-                    row['tmdb_id'],
-                    row['original_title'],
-                    row['overview'],
-                    row['popularity'],
-                    datetime.datetime.strptime(row['release_date'], '%m/%d/%Y'),
-                    int(row['vote_count']),
-                    float(row['vote_average']),
-                ))
-
-        return movies_list
--- movie/views.py
+++ movie/views.py
@@ -5,19 +5,18 @@ from django.http import HttpResponseNotFound
 
 # Create your views here.
 def all_movies(request):
-    found_movies = Movie.find_all_movies()
+    found_movies = Movie.objects.all()
     context = {
         'movies': found_movies
     }
     return render(request, 'movie/all_movies.html', context)
 
 
-def movie_details(request, tmdb_id):
-    found_movie = Movie.find_all_movies()
-    for m in found_movie:
-        if m.tmdb_id == tmdb_id:
-            context = {
-                'movie': m
-            }
-            return render(request, 'movie/movie_details.html', context)
-    return HttpResponseNotFound('Film nie został znaleziony')
+def movie_details(request, id):
+    found_movie = Movie.objects.get(pk=id)
+    if not found_movie:
+        return HttpResponseNotFound('Film nie został znaleziony')
+    context = {
+        'movie': found_movie
+    }
+    return render(request, 'movie/movie_details.html', context)
